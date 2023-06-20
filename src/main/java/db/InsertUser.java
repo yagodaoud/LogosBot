0ce52247a92e37076e1cea5ee9d293056e8e3b97
Yago Daoud <yagodaoud@hotmail.com>
2023-06-19 23:02:48 -0300

    Add insertUser first method

--- src/main/java/db/InsertUser.java
+++ src/main/java/db/InsertUser.java
@@ -1,5 +1,21 @@
 package main.java.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class InsertUser {
+
+    public static void main(String[] args) throws SQLException {
+        InsertUser insertUser = new InsertUser();
+        insertUser.insertUser();
+    }
+    public void insertUser() throws SQLException {
+        Connection connector = MySQLConnector.getConnection();
+        String sql = "INSERT INTO users (user_name) VALUES ('test')";
+        PreparedStatement statement = connector.prepareStatement(sql);
+        statement.execute();
+        connector.close();
+    }
 
 }
--- src/main/java/db/MySQLConnector.java
+++ src/main/java/db/MySQLConnector.java
@@ -4,28 +4,18 @@ import java.sql.*;
 public class MySQLConnector {
 
     public static Connection getConnection() throws SQLException {
-
-        final String dbConnection = "jdbc:mysql://localhost";
+        final String dbConnection = "jdbc:mysql://localhost:3306/discordbot";
         final String dbUser = "root";
         final String dbPassword = "1234";
         Connection connection = null;
 
         try {
             connection = DriverManager.getConnection(dbConnection, dbUser, dbPassword);
-            System.out.println("Successfully connected to db");
-
+            System.out.println("Successfully connected to the database");
         } catch (SQLException e) {
             System.out.println("Connection failed");
-        } finally {
-            if (connection != null){
-                try {
-                    connection.close();
-                    System.out.println("Connection closed");
-                } catch (SQLException e){
-                    System.out.println("Failed to close connection");
-                }
-            }
         }
+
         return connection;
     }
 }
