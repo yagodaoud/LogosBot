06e670536449a4bff4e1f7a8a62e4a75c26d1d92
Yago Daoud <yagodaoud@hotmail.com>
2023-06-25 23:09:46 -0300

    Setup test class

--- test/JDATest.java
+++ test/JDATest.java
@@ -1,25 +1,49 @@
 import main.java.DiscordBot;
+import main.java.commands.BotCommands;
+import main.java.crypto.CryptoPriceDiscord;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import javax.security.auth.login.LoginException;
+import static org.mockito.Mockito.verify;
+
 
 public class JDATest {
+    @Mock
+    static DiscordBot bot;
 
-    private static DiscordBot bot;
+    @Mock
+    static SlashCommandInteractionEvent slashEvent;
 
-    @BeforeAll
-    public static void setup() throws LoginException {
-        bot = new DiscordBot();
+    @Mock
+    static CryptoPriceDiscord cryptoPrice;
+
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    public void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+        Mockito.when(CryptoPriceDiscord.getPrice(Mockito.anyString())).thenReturn(100.0);
     }
 
     @Test
-    public void testBot() throws LoginException {
+    public void testCryptoCommand() {
+        setup();
+        beforeEach();
+        BotCommands botCommands = new BotCommands();
+        botCommands.onSlashCommandInteraction(slashEvent);
+        CryptoPriceDiscord.getPrice("BTC");
+
     }
+
     @AfterAll
     public static void cleanup() {
         bot.shutdown();
-        }
-
+    }
 }
