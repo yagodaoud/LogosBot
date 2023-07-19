0a46eb1bae7d5845cd90092683fd7e467c89fd33
Yago Daoud <yagodaoud@hotmail.com>
2023-07-19 14:43:43 -0300

    Update InsertUser to be triggered every hour

--- src/main/java/db/InsertUser.java
+++ src/main/java/db/InsertUser.java
@@ -4,32 +4,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class InsertUser {
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private static final long ALERT_INTERVAL= 3600;
+
     public static void insertUsers() throws SQLException {
-        Connection connector = MySQLConnector.getConnection();
-        List<String> userList = UserListGetter.membersValue();
+        executorService.scheduleAtFixedRate(() -> {
+            try {
 
-        String sql = "INSERT INTO users (user_name) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM users WHERE user_name = ?)";
+                Connection connector = MySQLConnector.getConnection();
+                List<String> userList = UserListGetter.membersValue();
 
-        PreparedStatement statement = connector.prepareStatement(sql);
+                String sql = "INSERT INTO users (user_name) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM users WHERE user_name = ?)";
 
-        for (String userName : userList) {
-            statement.setString(1, userName);
-            statement.setString(2, userName);
-            statement.addBatch();
-        }
+                PreparedStatement statement = connector.prepareStatement(sql);
 
-        int[] result = statement.executeBatch();
-        connector.close();
+                for (String userName : userList) {
+                    statement.setString(1, userName);
+                    statement.setString(2, userName);
+                    statement.addBatch();
+                }
 
-        int numInserted = 0;
-        for (int i : result) {
-            if (i == PreparedStatement.SUCCESS_NO_INFO || i > 0) {
-                numInserted++;
+                int[] result = statement.executeBatch();
+                connector.close();
+
+                int numInserted = 0;
+                for (int i : result) {
+                    if (i == PreparedStatement.SUCCESS_NO_INFO || i > 0) {
+                        numInserted++;
+                    }
+                }
+
+                System.out.println(numInserted + " users inserted.");
+            } catch (SQLException exception){
+                System.out.println(exception.getMessage());
             }
-        }
 
-        System.out.println(numInserted + " users inserted.");
+        },0, ALERT_INTERVAL, TimeUnit.SECONDS);
     }
 }
